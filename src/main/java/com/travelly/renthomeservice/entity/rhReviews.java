2ccfb9bd3f7d9e82039596e0dc254335cd2208a8
srooodg <unitsin@yandex.ru>
2023-06-29 18:57:31 +0300

    не работает

---
+++ src/main/java/com/travelly/renthomeservice/entity/rhReviews.java
@@ -0,0 +1,39 @@
+package com.travelly.renthomeservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+
+@Entity
+@Data
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@Table(name = "company_information", schema = "company")
+public class rhReviews {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "property_id")
+    @GeneratedValue
+    private UUID propertyId;
+
+    @Column(name = "rating", columnDefinition = "NUMERIC(5,2)")
+    private Double rating;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "created_at")
+    @NonNull
+    private Timestamp createdAt;
+
+}
