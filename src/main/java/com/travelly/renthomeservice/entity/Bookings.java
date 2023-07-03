92bfa21e4563e3d9bcadb282c09e58b6bc1fbc44
Anastasiya Zinnyatullova <nastyaplv@mail.ru>
2023-07-03 17:48:19 +0300

    feat: add rh_bookings and migrations

---
+++ src/main/java/com/travelly/renthomeservice/entity/Bookings.java
@@ -0,0 +1,45 @@
+package com.travelly.renthomeservice.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity //исправить импорт
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "rh_bookings")
+public class Bookings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "booking_id")
+    private UUID id;
+
+    @JoinColumn(name = "property_id")
+    private UUID propertyId;
+
+    @Column(name = "check_in_date")
+    private Timestamp checkInDate;
+
+    @Column(name = "check_out_date")
+    private Timestamp checkOutDate;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name = "payment_status")
+    private String paymentStatus;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+}
