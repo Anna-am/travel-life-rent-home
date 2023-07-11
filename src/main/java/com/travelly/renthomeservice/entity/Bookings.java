a75f9803d03f5021d21eec64841bb267f2198184
nastyaplv <104436223+nastyaplv@users.noreply.github.com>
2023-07-11 08:27:43 +0300

    Feature/rent home service 2 (#5)
    rent home service 2

---
+++ src/main/java/com/travelly/renthomeservice/entity/Bookings.java
@@ -0,0 +1,44 @@
+package com.travelly.renthomeservice.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "rh_bookings")
+public class Bookings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private UUID id;
+
+    @JoinColumn(name = "property_id")
+    private UUID propertyId;
+
+    @Column(name = "check_in_date")
+    private Timestamp checkInDate;
+
+    @Column(name = "check_out_date")
+    private Timestamp checkOutDate;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name = "payment_status")
+    private String paymentStatus;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+}
