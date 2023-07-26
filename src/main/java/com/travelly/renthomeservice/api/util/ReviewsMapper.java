96bfdd0bb117be61cb38a4a24b1443cbd5f24d74
Sergei <97890676+96test96@users.noreply.github.com>
2023-07-26 19:18:50 +0300

    feature/RentHome-service-6 (#14)
    Исправил замечания в ReviewsService

---
+++ src/main/java/com/travelly/renthomeservice/api/util/ReviewsMapper.java
@@ -0,0 +1,18 @@
+package com.travelly.renthomeservice.api.util;
+
+import com.travelly.renthomeservice.api.dto.ReviewsDtoRequest;
+import com.travelly.renthomeservice.entity.Reviews;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ReviewsMapper {
+
+    private final ModelMapper modelMapper;
+
+    public Reviews convertToEntity(ReviewsDtoRequest reviewsDtoRequest) {
+        return modelMapper.map(reviewsDtoRequest, Reviews.class);
+    }
+}
